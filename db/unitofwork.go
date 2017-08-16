8953a683f7a6d09548abb644544bdaf32cebbc29
Helder Farias <helder.farias@dataprev.gov.br>
2017-08-16 15:54:34 -0300

    Add transaction support

--- db/unitofwork.go
+++ db/unitofwork.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"errors"
 
 	"github.com/jmoiron/sqlx"
 )
@@ -9,11 +10,18 @@ import (
 //UnitOfWork wrapper tx
 type UnitOfWork interface {
 	MustNamedExec(query string, arg interface{}) sql.Result
+
 	Query(query string, args ...interface{}) (*sqlx.Rows, error)
+
 	Select(dest interface{}, query string, args ...interface{}) error
+
 	NamedQuery(query string, arg interface{}) (*sqlx.Rows, error)
+
 	MustExec(query string, args ...interface{}) sql.Result
+
 	Get(dest interface{}, query string, args ...interface{}) error
+
+	InTransaction(contextOver func(db UnitOfWork) error)
 }
 
 type unitOfWork struct {
@@ -24,6 +32,25 @@ type unitOfWork struct {
 //NewUnitOfWork factory method
 func NewUnitOfWork(db *sqlx.DB, tx *sqlx.Tx) UnitOfWork {
 	return &unitOfWork{db: db, tx: tx}
+}
+
+func (u *unitOfWork) InTransaction(contextOver func(db UnitOfWork) error) {
+	u.begin()
+
+	defer func() {
+		if r := recover(); r != nil {
+			u.rollback()
+			panic(r)
+		}
+	}()
+
+	err := contextOver(u)
+
+	if err == nil {
+		u.commit()
+	} else {
+		u.rollback()
+	}
 }
 
 func (u *unitOfWork) MustNamedExec(query string, arg interface{}) sql.Result {
@@ -82,4 +109,38 @@ func (u *unitOfWork) Get(dest interface{}, query string, args ...interface{}) er
 	}
 
 	return u.db.Get(dest, query, args...)
+}
+
+func (u *unitOfWork) begin() {
+	u.tx = u.db.MustBegin()
+
+	if u.tx == nil {
+		panic(errors.New("Nenhuma transação foi iniciada."))
+	}
+}
+
+func (u *unitOfWork) commit() {
+	if u.tx == nil {
+		panic(errors.New("Nenhuma transação foi iniciada."))
+	}
+
+	err := u.tx.Commit()
+	if err != nil {
+		panic(err)
+	}
+
+	u.tx = nil
+}
+
+func (u *unitOfWork) rollback() {
+	if u.tx == nil {
+		panic(errors.New("Nenhuma transação foi iniciada."))
+	}
+
+	err := u.tx.Rollback()
+	if err != nil {
+		panic(err)
+	}
+
+	u.tx = nil
 }
