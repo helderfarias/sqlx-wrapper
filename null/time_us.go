bf2f69c36b087fb182bc801ff340e06c56d18d53
Helder Farias <helder.farias@dataprev.gov.br>
2019-03-22 11:03:28 -0300

    Adicionar  time

---
+++ null/time_us.go
@@ -0,0 +1,130 @@
+package null
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
+)
+
+const (
+	TIME_FORMAT_HHMMSS = "15:04:05"
+)
+
+type Time struct {
+	Date  time.Time
+	Valid bool
+}
+
+func TimeFrom(src string) Time {
+	date, err := time.Parse(TIME_FORMAT_HHMMSS, src)
+	if err != nil {
+		fallback := time.Now()
+		return Time{
+			Date:  time.Date(0, 0, 0, fallback.Hour(), fallback.Minute(), fallback.Second(), fallback.Nanosecond(), time.UTC),
+			Valid: false,
+		}
+	}
+
+	return NewTime(date)
+}
+
+func NewTime(date time.Time) Time {
+	return Time{
+		Date:  time.Date(0, 0, 0, date.Hour(), date.Minute(), date.Second(), date.Nanosecond(), time.UTC),
+		Valid: true,
+	}
+}
+
+func (n *Time) Scan(value interface{}) error {
+	n.Date, n.Valid = value.(time.Time)
+	return nil
+}
+
+func (n Time) Value() (driver.Value, error) {
+	if !n.Valid {
+		return nil, nil
+	}
+	return n.Date, nil
+}
+
+func (n Time) MarshalJSON() ([]byte, error) {
+	if !n.Valid {
+		return []byte("null"), nil
+	}
+
+	t := n.Date
+
+	if y := t.Year(); y < 0 || y >= 10000 {
+		return nil, errors.New("Date.MarshalJSON: year outside of range [0,9999]")
+	}
+
+	value := t.Format(TIME_FORMAT_HHMMSS)
+	bytes := []byte(fmt.Sprintf("\"%s\"", value))
+	return bytes, nil
+}
+
+func (n *Time) UnmarshalJSON(data []byte) (err error) {
+	type dateDecode struct {
+		Date  string
+		Valid bool
+	}
+
+	if string(data) == "" || string(data) == "null" {
+		n.Date = time.Time{}
+		n.Valid = false
+		err = nil
+		return
+	}
+
+	var decode dateDecode
+	err = json.Unmarshal(data, &decode.Date)
+
+	n.Date, err = time.Parse(TIME_FORMAT_HHMMSS, string(decode.Date))
+	n.Valid = decode.Valid
+	n.Valid = err == nil
+	return
+}
+
+func (n *Time) UnmarshalText(text []byte) error {
+	str := string(text)
+	if str == "" || str == "null" {
+		n.Valid = false
+		return nil
+	}
+
+	var err error
+	n.Date, err = time.Parse(TIME_FORMAT_HHMMSS, str)
+	n.Valid = err == nil
+	return err
+}
+
+func (n Time) MarshalText() ([]byte, error) {
+	if !n.Valid {
+		return []byte{}, nil
+	}
+
+	t := n.Date
+
+	return []byte(t.Format(TIME_FORMAT_HHMMSS)), nil
+}
+
+func (d Time) IsZero() bool {
+	return !d.Valid
+}
+
+func (d *Time) Equal(that *Time) bool {
+	return d.Date.Hour() == that.Date.Hour() &&
+		d.Date.Minute() == that.Date.Minute() &&
+		d.Date.Second() == that.Date.Second()
+}
+
+func (d *Time) String() string {
+	bytes, err := d.MarshalText()
+	if err != nil {
+		return ""
+	}
+
+	return string(bytes)
+}
---
+++ null/time_us_test.go
@@ -0,0 +1,108 @@
+package null
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseTimeString(t *testing.T) {
+	time1 := TimeFrom("12:56:22")
+	time2 := NewTime(time.Date(2015, time.January, 17, 10, 20, 05, 5, time.UTC))
+	time3 := TimeFrom("")
+
+	assert.Equal(t, "12:56:22", time1.String())
+	assert.Equal(t, "10:20:05", time2.String())
+	assert.Empty(t, time3.String())
+}
+
+func TestCriarTimeFrom(t *testing.T) {
+	data1 := TimeFrom("10:10:01")
+	data2 := NewTime(time.Date(2015, time.January, 17, 10, 10, 1, 0, time.UTC))
+
+	assert.NotNil(t, data1)
+	assert.Equal(t, data1, data2)
+	assert.True(t, data1.Valid)
+}
+
+func TestCriarTimeFromInvalidaSeInputVazio(t *testing.T) {
+	data1 := TimeFrom("")
+	data2 := NewTime(time.Date(2015, time.January, 17, 0, 0, 0, 0, time.UTC))
+
+	assert.NotNil(t, data1)
+	assert.NotEqual(t, data1, data2)
+	assert.False(t, data1.Valid)
+}
+
+func TestCriarTimeNulo(t *testing.T) {
+	t1 := Time{}
+
+	var value *time.Time
+	t1.Scan(value)
+
+	assert.False(t, t1.Valid)
+}
+
+func TestLerTimeNulo(t *testing.T) {
+	t1 := Time{}
+
+	var value *time.Time
+	t1.Scan(value)
+
+	lido, _ := t1.Value()
+
+	assert.Nil(t, lido)
+}
+
+func TestCriarTime(t *testing.T) {
+	t1 := Time{}
+
+	t1.Scan(time.Now())
+
+	assert.True(t, t1.Valid)
+}
+
+func TestConverterTimeParaJson(t *testing.T) {
+	t1 := Time{}
+
+	agora := time.Date(2015, time.April, 17, 10, 10, 10, 0, time.UTC)
+	t1.Scan(agora)
+
+	bytes, err := t1.MarshalJSON()
+
+	assert.Nil(t, err)
+	assert.Equal(t, `"10:10:10"`, string(bytes))
+}
+
+func TestConverterJsonParaTime(t *testing.T) {
+	agora := time.Date(2015, time.April, 17, 10, 10, 10, 0, time.UTC)
+
+	t1 := Time{}
+	t1.Scan(agora)
+
+	err := json.Unmarshal([]byte(`"10:10:10"`), &t1)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "10:10:10", t1.String())
+}
+
+func TestConverterJsonParaTimeNull(t *testing.T) {
+	agora := time.Date(2015, time.April, 17, 0, 0, 0, 0, time.UTC)
+
+	t1 := Time{}
+	t1.Scan(agora)
+
+	err := json.Unmarshal([]byte(`null`), &t1)
+
+	assert.Nil(t, err)
+	assert.False(t, t1.Valid)
+}
+
+func TestCompararTime(t *testing.T) {
+	t1 := NewTime(time.Date(2015, time.April, 17, 0, 0, 0, 0, time.UTC))
+	t2 := NewTime(time.Date(2015, time.April, 17, 0, 0, 0, 0, time.UTC))
+
+	assert.True(t, t1.Equal(&t2))
+}
